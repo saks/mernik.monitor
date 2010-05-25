b094be646dcf021a70c1319bfb8cff6a7a0c94df
saks <saksmlz@gmail.com>
2010-05-26 01:37:52 +0300

    counter state validation finished

--- src/chrome/content/mernik_monitor_class.js
+++ src/chrome/content/mernik_monitor_class.js
@@ -17,10 +17,12 @@ if ('undefined' == typeof(Mernik._MonitorClass)) {
 				counterPresents:   false,
 				containerPresents: false,
 				imageLoaded:       false,
-				scriptLoaded:      false
+				scriptLoaded:      false,
+				siteId:            null,
+				status:            'no'
 			};
 
-			this.window   = DOMWindow;
+			this.window   = DOMWindow.wrappedJSObject;
 			this.document = this.window.document;
 			this.findMernikCounter()
 			dump("init mernik monitor\n")
@@ -33,29 +35,57 @@ if ('undefined' == typeof(Mernik._MonitorClass)) {
 
 
 			/* find image container */
-			if ('undefined' != typeof(imageContainer)) {
+			if (imageContainer) {
 				this.changeCounterState('containerPresents', true);
 			}
 
 			/* find mernik image */
 			if (imageContainer && imageContainer.firstChild) {
 				this.changeCounterState('imageLoaded', true);
+			} else if (this.filter(this.document.images, function(i) {
+				return i.src.indexOf(defaults.scriptServer) != -1;
+			})[0]) {
+				this.changeCounterState('imageLoaded', true);
 			}
 
 
 			/* find <script> tag with mernik counter */
-			this.mernikScript = Array.prototype.filter.call(scripts, function(i){
+			this.mernikScript = this.filter(scripts, function(i){
 				return (i.src.indexOf(self.defaults.scriptServer) != -1);
-			});
+			})[0];
 
 			if (this.mernikScript) {
 				this.changeCounterState('scriptLoaded', true);
+			};
+
+			/* get info from page variables */
+			if (this.window.mernik && this.window.mernik.id) {
+				this.changeCounterState('siteId', this.window.mernik.id);
+			};
+
+
+			/* parse page for mernik counter */
+			if (/mernik\scounter\sstart/.test(this.document.body.innerHTML)) {
+				this.changeCounterState('counterPresents', true);
+			};
+
+			/* deside about counter status */
+			if (this.counterState.siteId &&
+				this.counterState.imageLoaded &&
+				this.counterState.containerPresents ) {
+					this.changeCounterState('status', 'ok');
+			} else if (this.counterState.counterPresents) {
+				this.changeCounterState('status', 'wrong');
 			};
 
 		},
 
 		changeCounterState: function(key, value) {
 			this.counterState[key] = value;
+		},
+
+		filter: function(array, callBack) {
+			return Array.prototype.filter.call(array, callBack);
 		},
 
 		$: function(id) {
