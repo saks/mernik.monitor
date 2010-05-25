b0111af8477248ac236638c90439db07ff8956b8
saks <saksmlz@gmail.com>
2010-05-26 00:10:20 +0300

    stable onload detection!!!

---
+++ src/chrome/content/mernik_monitor_class.js
@@ -0,0 +1,15 @@
+if ('undefined' == typeof(Mernik._MonitorClass)) {
+	Mernik._MonitorClass = function() {
+		this.init();
+	};
+
+	Mernik._MonitorClass.prototype = {
+
+		init: function() {
+			dump('init mernik monitor')
+		}
+
+	}; /* end of prototype definition */
+
+};
+
