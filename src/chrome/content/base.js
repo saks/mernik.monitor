fa108887f4df5ebf3d2a7ea40fd6bd8580922215
saks <saksmlz@gmail.com>
2010-05-30 21:13:02 +0300

    logging refactored

--- src/chrome/content/base.js
+++ src/chrome/content/base.js
@@ -1,3 +1,27 @@
 /* initialize Mernik namespace if it was not initialized */
 if ('undefined' == typeof(Mernik)) var Mernik = {};
+Mernik.DEBUG = true;
+
+function log(text) {
+	if (!Mernik.DEBUG) return;
+
+	var result = "";
+
+	if ('string' == typeof(text)) {
+		result += text
+	} else if ('object' == typeof(text)) {
+		var pairs = [];
+		for (k in text) {
+			pairs.push("" + k + ": " + text[k])
+		}
+		result += ("<" + text.constructor + "> [" + pairs.join(', ') + "]")
+	} else if ('undefined' == typeof(text)){
+		result += "undefined";
+	} else {
+		result += "unknown type: " + typeof(text)
+	};
+
+	dump(result); dump("\n")
+
+}
 
