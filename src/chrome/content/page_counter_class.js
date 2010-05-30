d2ea673dee12f94804df7b98585f50292e03490a
saks <saksmlz@gmail.com>
2010-05-30 20:21:51 +0300

    unknown and loading types of page counters added

--- src/chrome/content/page_counter_class.js
+++ src/chrome/content/page_counter_class.js
@@ -1,7 +1,7 @@
 if ('undefined' == typeof(Mernik._PageCounterClass)) {
 	(function(globalNamespace){
 		var MernikNamespace = globalNamespace.Mernik,
-			supportedIDS = ['mernik', 'akavita'];
+			supportedIDS = ['unknown', 'loading', 'mernik', 'akavita'];
 
 		Mernik._PageCounterClass = function(id, params) {
 			if (supportedIDS.indexOf(id) == -1)
@@ -14,18 +14,29 @@ if ('undefined' == typeof(Mernik._PageCounterClass)) {
 
 		Mernik._PageCounterClass.prototype = {
 			init: function(params) {
-				if ('mernik' == this.id) {
+				if ('unknown' == this.id) {
+					/* unknown state for mernik monitor */
+					this.name    = 'unknown'
+					this.siteId  = -1;
+					this.statURL = '';
+				} else if ('loading' == this.id) {
+					/* loading state for mernik monitor */
+					this.name    = 'loading'
+					this.siteId  = -1;
+					this.statURL = '';
+				} else if ('mernik' == this.id) {
+					/* mernik counter */
 					this.name    = 'mernik';
 					this.siteId  = params.window.SID;
 					this.statURL = 'http://top.mernik.by';
 				} else if ('akavita' == this.id) {
+					/* akavita counter */
 					this.name    = 'akavita';
 					this.siteId  = params.window.AC_ID;
 
 					/* parce site id from page source for old counter version */
-					var re = /lik\?id=(\d+)/
-					if (!this.siteId && re.test(params.pageHTML)) {
-						this.siteId = re.exec(params.pageHTML)[1];
+					if (!this.siteId && Mernik._PageCounterClass.AKAVITA_RE1.test(params.pageHTML)) {
+						this.siteId = Mernik._PageCounterClass.AKAVITA_RE1.exec(params.pageHTML)[1];
 					};
 
 					this.statURL = 'http://stat.akavita.com/stat/stat.pl?id=' + this.siteId + '&lang=ru';
@@ -36,8 +47,9 @@ if ('undefined' == typeof(Mernik._PageCounterClass)) {
 
 		/* constants */
 
-		Mernik._PageCounterClass.MERNIK_RE = /mernik\scounter\sstart/;
-		Mernik._PageCounterClass.AKAVITA_RE = /Akavita\scounter/;
+		Mernik._PageCounterClass.MERNIK_RE   = /mernik\scounter\sstart/;
+		Mernik._PageCounterClass.AKAVITA_RE  = /Akavita\scounter/;
+		Mernik._PageCounterClass.AKAVITA_RE1 = /lik\?id=(\d+)/;
 
 	}(window))
 }
